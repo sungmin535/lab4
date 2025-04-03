31cc73ec50bbcc427edfcbc4237ecb2543b69b5d
Sungmin Park <C27Sungmin.Park@afacademy.af.edu>
2025-04-03 11:03:24 -0600

    lab4

---
+++ src/elevator_fsm.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/03/2025 10:21:35 AM
+-- Design Name: 
+-- Module Name: elevator_fsm - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity elevator_fsm is
+--  Port ( );
+end elevator_fsm;
+
+architecture Behavioral of elevator_fsm is
+
+begin
+
+
+end Behavioral;
---
+++ src/elevator_fsm_tb.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/03/2025 10:22:33 AM
+-- Design Name: 
+-- Module Name: elevator_fsm_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity elevator_fsm_tb is
+--  Port ( );
+end elevator_fsm_tb;
+
+architecture Behavioral of elevator_fsm_tb is
+
+begin
+
+
+end Behavioral;
